c1895091618b7a7e3160057b55190d8f3d5d5abf
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-16 22:22:25 +0100

    implemented JWT and first protected route

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://mike:mike666@ds259463.mlab.com:59463/project-devconnector"
+    "mongodb://mike:mike666@ds259463.mlab.com:59463/project-devconnector",
+  secretOrKey: "secret"
 };
