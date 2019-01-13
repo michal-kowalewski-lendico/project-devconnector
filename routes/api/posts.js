79342ce267dddbabf9f9301dc7d6f6699bf1f354
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-13 18:56:56 +0100

    added routes for get post, posts and delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const passport = require("passport");
 
 // Post model
 const Post = require("../../models/Post");
+
+// Profile module
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -15,10 +18,30 @@ const validatePostInput = require("../../validation/post");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with this id" })
+    );
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
-
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -37,6 +60,32 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        //Check post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+
+        // Delete
+        post
+          .remove()
+          .then(() => res.json({ succes: true }))
+          .catch(err =>
+            res.status(404).json({ postnotfound: "No post found" })
+          );
+      });
+    });
   }
 );
 
