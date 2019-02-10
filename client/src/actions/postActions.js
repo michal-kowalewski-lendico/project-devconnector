84f7412f549910fb0400dfc01d1f203712cb7f37
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-10 14:58:18 +0100

    added Posts and PostFeed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,9 +3,9 @@ import axios from "axios";
 import { ADD_POST, GET_ERRORS } from "./types";
 
 // Add post
-export const addPost = postData => dipatch => {
+export const addPost = postData => dispatch => {
   axios
-    .post("/api/post", postData)
+    .post("/api/posts", postData)
     .then(res =>
       dispatch({
         type: ADD_POST,
@@ -15,7 +15,7 @@ export const addPost = postData => dipatch => {
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.respons.data
+        payload: err.response.data
       })
     );
 };
