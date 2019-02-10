8992abb44d6495f712cdb09f4d97578412589848
MikeKowalsky <mkowalewski83@gmail.com>
2019-02-10 14:11:47 +0100

    fixed bug unhandled promise profileByHandle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -76,7 +76,7 @@ router.get("/handle/:handle", (req, res) => {
     .then(profile => {
       if (!profile) {
         errors.noprofile = "There is no profile for this handle";
-        res.status(404).json(errors);
+        return res.status(404).json(errors);
       }
       res.json(profile);
     })
